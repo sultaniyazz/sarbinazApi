6fcad394dbe7cced2414f5979e5319fae02540dc
Sultaniyazdev <sultaniyazdev@gmail.com>
2025-05-24 11:34:25 +0500

    qwerty

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // JSON faylingiz nomi
+const router = jsonServer.router('data.json'); // JSON faylingiz nomi
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 3000;
 
